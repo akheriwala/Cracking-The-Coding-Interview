5cd473a313b03b2941e7f416cf93e482ad3863fc
akheriwala <40525238+akheriwala@users.noreply.github.com>
2018-07-15 07:56:43 -0700

    Update nthToLastElement.java

--- Chapter2-LinkedList/nthToLastElement.java
+++ Chapter2-LinkedList/nthToLastElement.java
@@ -1,15 +1,17 @@
+/**
+ * Implement an algorithm to find the nth to last element of a singly linked list.
+ * Solution -
+ * 1. Find the nth element from the start of the LinkedList.
+ * 2. Head of the LinkedList is at index 0.
+ * 3. Move both the nth element & the head until the nth element reaches the end of the LinkedList or null.
+ * 4. The moved head of the Linkedlist is now the nth to the last element of the singly linked list.
+ */
+
 package com.chapter2;
+import helper.node;
 
 public class nthToLast {
 
-    static class node {
-        int data;
-        node next;
-
-        public node(int data) {
-            this.data = data;
-        }
-    }
 
     static node nthToLast(node head, int n) {
         if (head == null || n < 0) {
@@ -31,18 +33,13 @@ public class nthToLast {
         return head;
     }
 
+    //Test class
     public static void main(String[] args) {
-        nthToLast.node start = new nthToLast.node(10);
-        start.next = new nthToLast.node(20);
-        start.next.next = new nthToLast.node(30);
-        start.next.next.next = new nthToLast.node(40);
-        start.next.next.next.next = new nthToLast.node(50);
-        start.next.next.next.next.next = new nthToLast.node(60);
-        start.next.next.next.next.next.next = new nthToLast.node(70);
-        start.next.next.next.next.next.next.next = new nthToLast.node(80);
-        start.next.next.next.next.next.next.next.next = new nthToLast.node(90);
 
-        nthToLast(start, 4);
+          int[] list = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+          node n = node.buildList(list);
+
+        nthToLast(n, 4);
 
     }
 
