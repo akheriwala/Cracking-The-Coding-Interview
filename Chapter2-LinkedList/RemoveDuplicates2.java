0fa5075af4f76eaca66dd59cf038f1db26d47779
akheriwala <40525238+akheriwala@users.noreply.github.com>
2018-07-15 08:13:31 -0700

    Update RemoveDuplicates2.java

--- Chapter2-LinkedList/RemoveDuplicates2.java
+++ Chapter2-LinkedList/RemoveDuplicates2.java
@@ -1,14 +1,17 @@
-package com.chapter2;
-//Solution if extra buffer is not allowed.
-public class RemoveDups2 {
-    static class node {
-        int data;
-        node next;
+/**
+ * Write code to remove duplicates from an unsorted linked list.
+ *
+ * FOLLOW UP
+ * How would you solve this problem if a temporary buffer is not allowed?
+ *
+ * Below solution is implemented when a temporary buffer is not allowed.
+ */
 
-        public node(int data) {
-            this.data = data;
-        }
-    }
+package com.chapter2;
+import helper.node;
+
+
+public class RemoveDups2 {
 
     public static void removeDups2 (node head) {
         if (head == null) return;
@@ -33,30 +36,18 @@ public class RemoveDups2 {
         }
     }
 
-    //Method to Print LinkedList
-    private static void printList(node n) {
-        while (n != null) {
-            System.out.print(n.data + " ");
-            n = n.next;
-        }
-        System.out.println(" ");
-    }
-
+    //TEST -----------------------------------
     public static void main(String[] args) {
-        RemoveDups2.node start = new RemoveDups2.node(10);
-        start.next = new RemoveDups2.node(12);
-        start.next.next = new RemoveDups2.node(13);
-        start.next.next.next = new RemoveDups2.node(12);
-        start.next.next.next.next = new RemoveDups2.node(15);
-        start.next.next.next.next.next = new RemoveDups2.node(16);
-        start.next.next.next.next.next.next = new RemoveDups2.node(10);
+
+        int[] list = {10, 12, 13, 12, 15, 16, 10, 20, 13, 15};
+        node start = node.buildList(list);
 
         System.out.println("LinkedList before removing duplicates: ");
-        printList(start);
+        node.printList(start);
 
         removeDups2(start);
 
         System.out.println("LinkedList after removing duplicates: ");
-        printList(start);
+        node.printList(start);
     }
 }
