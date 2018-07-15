e56c086736a8d0f91c59b20a53991056ee98dd05
akheriwala <40525238+akheriwala@users.noreply.github.com>
2018-07-15 07:57:59 -0700

    Create LinkedList.java

---
+++ helper/LinkedList.java
@@ -0,0 +1,36 @@
+package helper;
+
+
+public class node {
+    public int data;
+    public node next;
+
+    public node(int data) {
+        this.data = data;
+    }
+
+    //Method to create a LinkedList from a given array
+    public static node buildList(int[] a) {
+        if (a == null || a.length == 0) return null;
+        node n = new node(a[0]);
+        node head = n;
+        for (int i = 1; i < a.length; i++) {
+            n.next = new node(a[i]);
+            n = n.next;
+        }
+        return head;
+    }
+
+    //Method to Print LinkedList
+    private static void printList(node n) {
+        while (n != null) {
+            System.out.print(n.data + "-->");
+            n = n.next;
+        }
+        System.out.println(" ");
+    }
+
+    public String toString() {
+        return String.valueOf(data);
+    }
+}
